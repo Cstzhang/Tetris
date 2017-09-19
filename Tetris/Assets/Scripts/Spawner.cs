f7ba2e1442f75714ad1cb3b4c81661190c9fbe08
Cstzhang <835066041@qq.com>
2017-09-19 11:36:48 +0800

    add  spawner

---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	//产生随机数，实例化模型
+	public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+		SpawnNext ();
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//生成
+	public void SpawnNext() {
+		int i = Random.Range (0, groups.Length);
+		GameObject ins = Instantiate (groups [i], transform.position, Quaternion.identity) as GameObject;
+
+
+
+
+	}
+
+
+}
