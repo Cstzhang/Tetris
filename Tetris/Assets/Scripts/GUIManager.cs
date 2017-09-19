b70dd8ff85e891b74f1d17d57fff0b0797e665ca
Cstzhang <835066041@qq.com>
2017-09-19 15:00:39 +0800

    添加计时器

---
+++ Tetris/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+	float gameTime,startTime;
+	Text timer;
+
+	// Use this for initialization
+	void Start () {
+		//计时器
+		timer = GameObject.Find("Canvas/Timer").GetComponent<Text>();
+		//得到游戏开始时间 s
+		startTime = Time.time;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	   //游戏时间 s
+		gameTime = Time.time - startTime;
+	   // s
+		int seconds = (int)(gameTime % 60); 
+	   // m
+		int minutes = (int)(gameTime / 60 );
+		// time string  Format
+		string strTime = string.Format("{0:00}:{1:00}",minutes,seconds);
+
+		timer.text = strTime;
+	}
+
+
+
+
+}
